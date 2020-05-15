8cf6c90ebd3d1b11518feb244c6a43df991baaad
Gibbyswag <will.gibson7@gmail.com>
2020-05-15 09:54:42 -0700

    added game functionality to add.js

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -12,7 +12,47 @@ class App extends Component {
     score: 0,
     highScores: 0,
     clicked: [],
-    text: "Take a Guess!"
+    text: "Pick a Puppy!"
+  };
+
+  // Function that determines whether picture was cicked before
+  clickMe = id => {
+    // If clicked id is in state, user loses 
+    if(this.state.clicked.includes(id)) {
+      this.setState({text:"You picked the same puppy :("})
+      // If high score is less than current score, set high score state as current score
+      if(this.state.highScore < this.state.score){
+        this.setState({highScore: this.state.score})
+      };
+      this.setState({clicked: []})
+      this.setState({score: 0})
+    }
+
+    // If clicked id is not in state, user continues to play
+    // Score in state increases
+    // Clicked id is set into clicked array in state
+
+    else{
+      this.setState({text: "You picked a new puppy!"})
+      this.setState({score: this.state.score + 1})
+      this.setState({clicked: [...this.state.clicked, id]})
+    };
+
+    const newClicked = this.state.images
+    this.shuffleImages(newClicked)
+  };
+  
+  // Shuffle images everytime an image is clicked
+  // Images passed as parameter
+
+  shuffleImages = newClicked => {
+    for(var i = newClicked.length -1; i > 0; i--) {
+      // Random number generated used to swap images
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = newClicked[i];
+      newClicked[i] = newClicked[j];
+      newClicked[j] = temp
+    };
   };
 
   render() {
