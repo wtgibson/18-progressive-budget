b31dbae3e93f44ead8f560ded0f17e739a0c23c4
Gibbyswag <will.gibson7@gmail.com>
2020-05-15 16:09:16 -0700

    Updates

---
+++ precache-manifest.7c24fbf2c315bc44547ea919b1a246d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da10059e10382d60209b0ec3cd246a5",
+    "url": "/18-clicky-game/index.html"
+  },
+  {
+    "revision": "ae5cef7e7b47530810fd",
+    "url": "/18-clicky-game/static/css/main.962d798b.chunk.css"
+  },
+  {
+    "revision": "763ea5f67d576495542c",
+    "url": "/18-clicky-game/static/js/2.af410d3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/18-clicky-game/static/js/2.af410d3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae5cef7e7b47530810fd",
+    "url": "/18-clicky-game/static/js/main.77298c51.chunk.js"
+  },
+  {
+    "revision": "2366dbfcfdd892949be5",
+    "url": "/18-clicky-game/static/js/runtime-main.83f39e50.js"
+  }
+]);
